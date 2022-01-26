07c1734210c72266248fdd70ed3ba6d6e71c6300
Nova <eldanadesalgne@gmail.com>
2022-01-25 22:44:15 -0500

    updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
--- Structure the DB 
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
@@ -16,15 +15,15 @@ CREATE TABLE roles(
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL, 
   departments_id INT,
-  FOREIGN KEY (departments_id) REFERENCES departments(id)
+  FOREIGN KEY (departments_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  roles_id INT NOT NULL,
+  roles_id INT,
   FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
   managers_id INT,
-  FOREIGN KEY (managers_id) REFERENCES employees(id) ON DELETE SET NULL
+  FOREIGN KEY (managers_id) REFERENCES employees(id) ON DELETE CASCADE
 );
